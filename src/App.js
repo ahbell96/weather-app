8ebbc6b81ff77e0dd604e0917a098a512db34417
Andrew Bell <andrew_h_bell@outlook.com>
2021-06-18 12:49:40 +0100

    addiition of material ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import {React, Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Grid, Button, Typography} from '@material-ui/core';
 
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <div className="">
-          Weather App
-        </div>
+        <Button variant="contained" color="primary">Hello World</Button>
       </div>
     );
   }
